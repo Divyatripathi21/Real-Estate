3d8b7c1e968bcd89c0f5c3cf5114df8f1761f3d7
Divya prakash tripathi <divytripathidivy@gmail.com>
2024-03-11 15:54:05 +0530

    connection to databases

--- api/index.js
+++ api/index.js
@@ -1,7 +1,15 @@
 import express from 'express'
+import mongoose from 'mongoose'
+
+mongoose.connect("mongodb://localhost:27017").then(()=>{
+  console.log("connected to db");
+})
+.catch((err)=>{
+  console.log(err);
+})
 
 const app=express();
 
 app.listen(3000,()=>{
-  console.log('server is running on port 3000');
+  console.log('server is running on port 3000')
 })
